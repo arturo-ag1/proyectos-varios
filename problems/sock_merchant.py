42082cb8efa9e0def3ef63d69741a187e1efd026
arturo-ag1 <arturo.aguileraromero@gmail.com>
2020-06-30 14:43:03 -0500

    Added description for Coding Challenge 1

--- problems/sock_merchant.py
+++ problems/sock_merchant.py
@@ -1,3 +1,10 @@
+#!/bin/python3
+
+#John works at a clothing store. He has a large pile of socks that he must pair by color for sale.
+# Given an array of integers representing the color of each sock, determine how many pairs of socks
+# with matching colors there are.
+
+
 import math
 
 sockList = [10,20,20,10,10,30,50,10,20]
