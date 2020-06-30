d74aa82a9b8bbb47faa01a90bdc455af88a8c4e7
arturo-ag1 <arturo.aguileraromero@gmail.com>
2020-06-30 14:50:42 -0500

    New way to solve Coding Challenge 1

--- problems/sock_merchant.py
+++ problems/sock_merchant.py
@@ -11,6 +11,8 @@ sockList = [10,20,20,10,10,30,50,10,20]
 socksIgnored = []
 socksNumber = 9
 
+#  Way 1 - Changing every occurrence of an iterated number to a hyphen (-)
+# First way of resolving the problem, adds innecesary values to the tmp list
 def setHyphens(sockNumber,tmpSockList):
 	amount = 0
 	for i in range(len(tmpSockList)):
@@ -18,20 +20,26 @@ def setHyphens(sockNumber,tmpSockList):
 			tmpSockList[i] = "-"
 
 
+# Way 2 - Deleting every occurrence of an interated number.
+# Deletes innecesary iterations to the tmp list
+def deleteValues(sock, tmpSockList):
+	while sock in tmpSockList:
+		tmpSockList.remove(sock)
+
+
 def sockMerchant(socksNumber,sockList):
 	tmpSockList = sockList.copy()
 	sockPairs = 0
 	for sock in tmpSockList:
-		if(sock != "-"):
-			if math.floor(tmpSockList.count(sock)/2) > 0:
-				sockPairs = sockPairs + math.floor(tmpSockList.count(sock)/2)
-				setHyphens(sock,tmpSockList)
+		if math.floor(tmpSockList.count(sock)/2) > 0:
+			sockPairs = sockPairs + math.floor(tmpSockList.count(sock)/2)
+			deleteValues(sock,tmpSockList)
 
 	return sockPairs
 
 sockPairs = sockMerchant(socksNumber,sockList)
 
-print("Socks Merchant:")
+print("Socks Merchant: \nFrom:")
 print(sockList)
-print("-----------")
+print("-----------\nTo:")
 print(sockPairs)
